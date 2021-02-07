d98e214cacee6aceaf2023b0686a8212375b393e
Dong Liu <dliu.cn@gmail.com>
2021-02-07 11:52:40 -0500

    use getopts

--- dense.sh
+++ dense.sh
@@ -1,7 +1,32 @@
-#!/bin/sh
-image_path=$1
-sparse_model=$2
-dense_workspace=${3:-$sparse_model/../dense}
+#!/bin/bash
+usage() {
+    echo "Usage:"
+    echo " dense.sh [ -i image_dir ] [ -s sparse_model ] [ -d dense_workspace ]"
+}
+image_path=""
+sparse_model=""
+dense_workspace=""
+
+while getopts "i:s:d:" options; do
+    case "${options}" in
+	i)
+	    image_path=${OPTARG}
+	    ;;
+	s)
+	    sparse_model=${OPTARG}
+	    ;;
+	d)
+	    dense_workspace=${OPTARG}
+	    ;;
+        *)
+            echo "error: unrecognized option ($arg)" 1>&2
+            usage
+            exit 1
+    esac
+done
+if [ -z "$dense_workspace" ]; then
+    dense_workspace=${sparse_model}/../dense
+fi
 
 colmap image_undistorter --image_path $image_path --input_path $sparse_model --output_path $dense_workspace --output_type COLMAP --max_image_size 2000
 colmap patch_match_stereo --workspace_path $dense_workspace --workspace_format COLMAP --PatchMatchStereo.geom_consistency true
