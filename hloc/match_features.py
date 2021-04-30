79b3e6cca9c57162114af1493a72c94dece713e1
Dong Liu <dliu.cn@gmail.com>
2021-04-30 12:22:44 -0400

    add best_match

--- hloc/match_features.py
+++ hloc/match_features.py
@@ -5,6 +5,7 @@ import h5py
 import logging
 from tqdm import tqdm
 import pprint
+import numpy as np
 
 from . import matchers
 from .utils.base_model import dynamic_load
@@ -41,6 +42,99 @@ def get_model(conf):
     Model = dynamic_load(matchers, conf['model']['name'])
     model = Model(conf['model']).eval().to(device)
     return model
+
+@torch.no_grad()
+def best_match(conf, global_feature_path, feature_path, match_output_path, num_match_required=10, max_try=None,
+              pair_file_path=None, match_sequential=False, sample_list=None, min_match_score=0.85, min_valid_ratio=0.09):
+    logging.info('Dyn Matching local features with configuration:'
+                 f'\n{pprint.pformat(conf)}')
+
+
+    assert global_feature_path.exists(), feature_path
+    global_feature_file = h5py.File(str(global_feature_path), 'r')
+
+    assert feature_path.exists(), feature_path
+    feature_file = h5py.File(str(feature_path), 'r')
+
+    match_file = h5py.File(str(match_output_path), 'a')
+
+    # get all sample names
+    if sample_list is not None:
+        names = sample_list
+    else:
+        names = []
+        global_feature_file.visititems(
+            lambda _, obj: names.append(obj.parent.name.strip('/'))
+            if isinstance(obj, h5py.Dataset) else None)
+        names = list(set(names))
+        names.sort()
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    def tensor_from_names(names, hfile):
+        desc = [hfile[i]['global_descriptor'].__array__() for i in names]
+        desc = torch.from_numpy(np.stack(desc, 0)).to(device).float()
+        return desc
+
+    desc = tensor_from_names(names, global_feature_file)
+    # descriptors are normalized, dot product indicates how close they are
+    sim = torch.einsum('id,jd->ij', desc, desc)
+    if max_try is None:
+        max_try = len(names)
+    topk = torch.topk(sim, max_try, dim=1).indices.cpu().numpy()
+
+    Model = dynamic_load(matchers, conf['model']['name'])
+    model = Model(conf['model']).eval().to(device)
+
+    pairs = []
+    matched = set()
+    for name0, indices in tqdm(zip(names, topk)):
+        num_matches_found = 0
+        for i in indices:
+            name1 = names[i]
+            if name0 != name1 :
+                pair = names_to_pair(name0, name1)
+
+                # Avoid to recompute duplicates to save time
+                if len({(name0, name1), (name1, name0)} & matched) \
+                   or pair in match_file:
+                    continue
+                data = {}
+                feats0, feats1 = feature_file[name0], feature_file[name1]
+                for k in feats1.keys():
+                    data[k+'0'] = feats0[k].__array__()
+                for k in feats1.keys():
+                    data[k+'1'] = feats1[k].__array__()
+                data = {k: torch.from_numpy(v)[None].float().to(device)
+                        for k, v in data.items()}
+
+                # some matchers might expect an image but only use its size
+                data['image0'] = torch.empty((1, 1,)+tuple(feats0['image_size'])[::-1])
+                data['image1'] = torch.empty((1, 1,)+tuple(feats1['image_size'])[::-1])
+
+                pred = model(data)
+                matches = pred['matches0'][0].cpu().short().numpy()
+                scores = pred['matching_scores0'][0].cpu().half().numpy()
+                # if score < min_match_score, set match to invalid
+                matches[ scores < min_match_score ] = -1
+                num_valid = np.count_nonzero(matches > -1)
+                if float(num_valid)/len(matches) > min_valid_ratio:
+                    pairs.append((name0, name1))
+                    grp = match_file.create_group(pair)
+                    grp.create_dataset('matches0', data=matches)
+                    grp.create_dataset('matching_scores0', data=scores)
+                    matched |= {(name0, name1), (name1, name0)}
+                    num_matches_found += 1
+                    if num_matches_found >= num_match_required:
+                        break
+        if num_matches_found < num_match_required:
+            logging.warning(f'num match for {name0} found {num_matches_found} less than num_match_required:{num_match_required}')
+
+    match_file.close()
+    if pair_file_path is not None:
+        with open(str(pair_file_path), 'w') as f:
+            f.write('\n'.join(' '.join([i, j]) for i, j in pairs))
+    logging.info('Finished exporting matches.')
 
 @torch.no_grad()
 def main(conf, pairs, features, export_dir, db_features=None, query_features=None, output_dir=None, exhaustive=False):
@@ -134,18 +228,35 @@ def main(conf, pairs, features, export_dir, db_features=None, query_features=Non
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--export_dir', type=Path, required=True)
+    parser.add_argument('--export_dir', type=Path)
     parser.add_argument('--output_dir', type=Path, required=False)
     parser.add_argument('--features', type=str,
                         default='feats-superpoint-n4096-r1024')
-    parser.add_argument('--db_features', type=Path, required=False)
+    parser.add_argument('--db_features', type=Path)
     parser.add_argument('--query_features', type=Path, required=False)
 
-    parser.add_argument('--pairs', type=Path, required=True)
+    parser.add_argument('--pairs', type=Path)
     parser.add_argument('--conf', type=str, default='superglue',
                         choices=list(confs.keys()))
     parser.add_argument('--exhaustive', action='store_true')
+
+    # best_match
+    parser.add_argument('--best_match', action='store_true')
+    parser.add_argument('--global_feature_path', type=Path)
+    parser.add_argument('--feature_path', type=Path)
+    parser.add_argument('--match_output_path', type=Path)
+    parser.add_argument('--num_match_required', type=int, default=10)
+    parser.add_argument('--max_try', type=int)
+    parser.add_argument('--min_match_score', type=float, default=0.85)
+    parser.add_argument('--min_valid_ratio', type=float, default=0.09)
+    parser.add_argument('--pair_file_path', type=Path)
+
     args = parser.parse_args()
-    main(
-        confs[args.conf], args.pairs, args.features,args.export_dir,
-        db_features=args.db_features, query_features=args.query_features, output_dir=args.output_dir, exhaustive=args.exhaustive)
+    if args.best_match:
+        best_match(confs[args.conf], args.global_feature_path, args.feature_path, args.match_output_path,
+                   num_match_required=args.num_match_required, min_match_score=args.min_match_score, min_valid_ratio=args.min_valid_ratio,
+                   max_try=args.max_try, pair_file_path=args.pair_file_path)
+    else:
+        main(
+            confs[args.conf], args.pairs, args.features,args.export_dir,
+            db_features=args.db_features, query_features=args.query_features, output_dir=args.output_dir, exhaustive=args.exhaustive)
